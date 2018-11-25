5b8513c77f2e874e56d91a8b924055fa666cb5b7
Ofir Cohen <ofir5300@gmail.com>
2018-11-25 15:09:49 +0200

    added member class

---
+++ Facebook/Member.hpp
@@ -0,0 +1,14 @@
+//
+//  Member.hpp
+//  Facebook
+//
+//  Created by Ofir Cohen on 25/11/2018.
+//  Copyright © 2018 Ilan Kushnir. All rights reserved.
+//
+
+#ifndef Member_hpp
+#define Member_hpp
+
+#include <stdio.h>
+
+#endif /* Member_hpp */
