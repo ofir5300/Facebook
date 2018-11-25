ee9242ccd89ca03207fe0ea3b70cc02428b1846b
Ilan Kushnir <ilan.kushnir@gmail.com>
2018-11-25 15:05:14 +0200

    Initial Commit

---
+++ Facebook/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Facebook
+//
+//  Created by Ilan Kushnir on 25/11/18.
+//  Copyright © 2018 Ilan Kushnir. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
