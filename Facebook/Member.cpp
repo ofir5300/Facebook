3fd613d2401d716da87a5efa1426b5b56f5bc6e8
Ofir Cohen <ofir5300@gmail.com>
2018-11-25 17:10:33 +0200

    member.cpp updated

--- Facebook/Member.cpp
+++ Facebook/Member.cpp
@@ -8,3 +8,33 @@
 
 #include "Member.hpp"
   
+const Status** Member:: getStatuses() const
+{
+    return (const Status**)statuses;
+}
+
+const Status** Member:: getRecentStatuses() const
+{
+    Status** recent = new Status* [10];
+    for(int i = 0 ; i < 10 ; i++)
+    {
+        recent[i] = statuses[i];
+    }
+    
+    return (const Status**)recent;
+}
+
+const Status** Member:: fetchFriendsStatuses() const
+{
+    
+}
+bool Member:: addFriend()
+{
+    
+}
+bool                addFanPage();
+Fanpage**           getFanPages()           const;
+Member**            getFriends()            const;
+char*               getName()               const;
+Date&               getBirthDate();
+Status**            getFriensRecentStatuses;
